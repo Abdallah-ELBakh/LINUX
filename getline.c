5a5a62db8e61716a3e3fd34b72de7b6502b42877
Abdallah Ayman <112198345+Abdallah-ELBakh@users.noreply.github.com>
2024-01-31 14:54:32 -0500

    Commit 1

---
+++ getline.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main (void)
+{
+	char* buffer = NULL;
+	size_t n =10;
+	while(1)
+	{
+		printf(":)");
+		getline(&buffer,&n,stdin);
+		printf("you said :%s\n",buffer);\
+		//remove the newline character in the entered cmd
+		buffer[strcspn(buffer,"\n")]='\0';
+		//check if the entered string is exit
+		if(strcmp(buffer , "exit") == 0)
+		{
+			printf(":( good bye\n");
+			break;
+		}
+		
+		
+	}
+	free(buffer);
+	return 0;
+
+}
